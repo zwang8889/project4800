6e5f2fce441c2bcdef58da45ff7e8f489a2e3fb0
zwang8889 <zi.wang@slu.edu>
2017-11-10 11:03:51 -0600

    Single variable rejection sampling

---
+++ R/oneDSample.R
@@ -0,0 +1,33 @@
+#' Single Variable Rejection Sampling
+#'
+#' This function implements single variable rehection sampling for rvs
+#' with bounded support and which have ahve bounded pdf.
+#' We expect N/maxf sampling function
+#'
+#' Finds up to N samples fron on rv with pdf f.
+#'
+#' Here are more details about the algorithm that we are using
+#'
+#'@param f the pdf that we are sampling from
+#'@param N the number of attempted samples
+#'@param lb the lower bound of support f
+#'@param nb the upper bound of support f
+#'@param mxaf bound on f
+#'
+#'@return A vector containing samples from pdf
+#'@export
+#'
+#'@examples
+#'
+#'betaPDF <- function(x){
+#'  ifelse(0<x&x <1, 2*x, 0)}
+#'oneDSAMPLE(f = betaPDF, N=100, lb=0, ub = 1, maxf = 2)
+#'hist(oneDSAMPLE(f = betaPDF, N=100, lb=0, ub = 1, maxf = 2))
+#'
+#'
+oneDSample <- function(f, N, lb, ub, maxf) {
+#  browser()
+  ones <- runif(N,lb,ub)
+  unis <- runif(N,0, maxf)
+  ones[unis < f(ones)]
+}
