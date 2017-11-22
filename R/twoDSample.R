cb1e21e63fca1673a1274b5ab6700528e6e228a8
zwang8889 <zi.wang@slu.edu>
2017-11-22 15:56:11 -0600

    set up for two-dim Sample

---
+++ R/twoDSample.R
@@ -0,0 +1,31 @@
+#' Double Variable Rejection Sampling
+#'
+#' This function implements two variables rejection sampling for rvs
+#' with bounded support and which have a bounded pdf.
+#'
+#' Here are more details about the algorithm that we are using
+#'
+#'@param f the pdf that we are sampling from
+#'@param N the number of attempted samples
+#'@param lbx the lower bound of support x
+#'@param lby the lower bound of support y
+#'@param ubx the upper bound of support x
+#'@param uby the upper bound of support y
+#'
+#'@return A vector containing samples from pdf
+#'@export
+#'
+#'@examples
+
+
+twoDsample <- function(fj, N, lbx, ubx, lby, uby) {
+
+  if (c(lbx,ubx), c(lby,uby), integral >0.01) {
+    stop("Error: not a pdf. The area under the function you given should be 1")
+  }
+  else{
+    for
+      }
+  }
+
+
