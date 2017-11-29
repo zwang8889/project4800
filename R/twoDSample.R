a05e1e2bdab9bb0a7ced6ae13af6bec5223e04b3
loreinayg <loreinayg@gmail.com>
2017-11-29 14:39:55 -0600

    To add up a sample to complement the comment part of twoDsample

--- R/twoDSample.R
+++ R/twoDSample.R
@@ -16,6 +16,12 @@
 #'@export
 #'
 #'@examples
+#'#'jointPDF <- function(x){
+#'x = x
+#'y = y
+#'ifelse(0<x & x <1 & 0<x & x<1 & 0<x+y & x+y<1, 2*x*y, 0)}
+#'twoDsample(f = jointPDF, N=100, lbx=0, ubx=1, lby=0, uby=1)
+#'
 
 
 twoDsample <- function(fj, N, lbx, ubx, lby, uby) {
