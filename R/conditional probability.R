b5c935aeb3d4ccb92519f841285aa87d2c6403e2
loreinayg <loreinayg@gmail.com>
2017-12-15 21:42:46 -0600

    updating the parameter name to match the expect value

--- R/conditional probability.R
+++ R/conditional probability.R
@@ -9,14 +9,18 @@
 #' @export as numbers
 #' 
 #
+f <- function(x) {ifelse(0 < x & x < 1, 4*x^3, 0)}
+x1 = oneDSample1(f,10000)
+c1 = function(x){x<0.4}
+probability(c1,x1)
 
-probability = function(condition,data){
-  length = length(data)
+probability = function(condition,rv){
+  length = length(rv)
   if(length==1){
-    return(mean(condition(data)))
+    return(mean(condition(rv)))
   }
   else if(length==2){
-    return(mean(condition(data$x,data$y)))
+    return(mean(condition(rv$x,rv$y)))
   }
   else{
     stop("This is not a valid formula.")
