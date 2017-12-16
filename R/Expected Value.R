56696b1ac52bf06850a71a80db74750f975f63d1
zwang8889 <zi.wang@slu.edu>
2017-12-15 20:27:18 -0600

    find the expeced val for one D sample

--- R/Expected Value.R
+++ R/Expected Value.R
@@ -1,6 +1,25 @@
 #' Single Variable Expected Value
 #'
-#' This function implements single variable rejection sampling for ramdon variables with bounded support
-#' and which have a bounded pdf.
+#' This function is an implementation of computing expected value for ramdon variables given pdf f.
+#'
+#' @param f the function that we will compute its expected value
+#' @param rv the random variabel will be given
+#'
+#' @return the expected value of the given function
+#' @export
+#'
+#' @examples
 #'
 #'
+#'
+#'
+#'
+
+Expected <- function(f,rv){
+  L <- length(rv)
+  if (len==1){
+    meanval <- mean(f(rv))
+    return(meanval)
+  }
+
+}
