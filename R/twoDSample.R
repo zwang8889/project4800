b01c4a823678e8284269cbd3f42d44542ee81132
loreinayg <loreinayg@gmail.com>
2017-12-16 20:09:29 -0600

    updating the multivariable integral & trying to fix first part.

--- R/twoDSample.R
+++ R/twoDSample.R
@@ -17,32 +17,23 @@
 #'@export
 #'@examples
 #'
- jointPFF <- function(x){
- x1 = x[1]
- x2 = x[2]
- ifelse(0<x1 & x1<1 & 0<x2 & x2<1 & 0<x1+x2 & x1+x2<1, 24*x1*x2, 0)}
- a <- twoDsample(f = jointPFF, lbx = 0, ubx = 1, lby = 0, uby = 1)
- ggplot(a, aes(x, y)) +  geom_density_2d()
 #'
-library(MASS)
-library(cubature)
+
 library(ggplot2)
 twoDsample <- function(f, N, lbx=-Inf, ubx=Inf, lby=-Inf, uby=Inf) {
-  integral = integrate(f,c(lbx,lby),c(ubx,uby)) $integral
+  integral = adaptIntegrate(f,c(lbx,lby),c(ubx,uby)) $integral
   if (0.98>integral|integral>1.01) {
    stop("Error: not a pdf. The area under the function you given should be 1")
  }
  else if(lbx!=-Inf&ubx!=Inf&lby!=-Inf&uby!=Inf){
-   maxf=max(f(c(runnif(5000,lbx,ubx)),c(runif(5000,lby,uby))))
-   sample = replicate(5000,{pSX=runnif(1,lbx,ubx);pSY=runif(1,lby,uby);if(runif(1,lbx,uby)<f(x=pSX,y=pSY))c(x=NA,y=NA)})
-   sampleframe=data.frame(t(sample))
- }
-  else{
-    x=runif(10000,-10000,10000)
-    y=runif(10000,-10000,10000)
-    maxf=
-    
-  }
-}
+   maxf <- max(replicate(100000,f(c(runif(1,lbx,ubx),runif(1,lby,uby)))))
+   twos=c()
+   pSX=runif(1,lbx,ubx)
+   pSY=runif(1,lby,uby)
+   two=c(pSX,pSY)
+   sample = data.frame(x=replicate(5000,{pSX;pSY;if(runif(1,0,maxf)<f(two)){
+  twos=c(twos,two)}}))
+ }}
+
 
 
