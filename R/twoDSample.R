669fcc1a2b213a6b3c5dc56f6fb01e4dfc608651
zwang8889 <zi.wang@slu.edu>
2017-12-16 23:57:03 -0600

    debug while loop error with missing para

--- R/twoDSample.R
+++ R/twoDSample.R
@@ -58,7 +58,7 @@ twoDsample <- function(f, N, lbx=-1000, ubx=1000, lby=-1000, uby=1000) {
 
     while (n<N){
       two = mvrnorm(n=1,mu,mat)
-      cond = C * d_norm(c(two, mu, c(sd,sd)))
+      cond = C * d_norm(two, mu, c(sd,sd))
       if ( runif(1,0, cond) < f(two)){
         twos = c(twos,two)
         n = n+1
