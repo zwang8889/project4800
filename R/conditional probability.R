1533b94fce27368342336987f047c83cad183115
loreinayg <loreinayg@gmail.com>
2017-12-16 20:21:31 -0600

    adding one more example, updating the na.rm

--- R/conditional probability.R
+++ R/conditional probability.R
@@ -11,8 +11,13 @@
 #
 #'f<- function(x) {ifelse(-1< x & x < 0, 3*x^2, 0)}
 #'w=oneDSample1(f,50000,-1,0)
-#'condition = function(x){x<0.4}
+#'condition = function(x){x<-0.4}
 #'probability(condition,w)
+#'
+#'f<- function(x) {1/pi/(1+x^2)}
+#'w=oneDSample1(f,50000)
+#'condition=function(x){x<4.5}
+#'probability(f,w)
 
 probability = function(condition,rv){
   length = length(rv)
