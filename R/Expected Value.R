79ca8b55a76453203d872186032bd855f58bd1d8
loreinayg <loreinayg@gmail.com>
2017-12-17 22:53:36 -0600

    fixing bugs & adding up expect falues for twoDsamples

--- R/Expected Value.R
+++ R/Expected Value.R
@@ -16,6 +16,22 @@
 #'f<- function(x) {1/pi/(1+x^2)}
 #'w=oneDSample1(f,50000)
 #'Expected(f,w)
+#'
+#'#jointPFF <- function(x){
+#x1 = x[1]
+#x2 = x[2]
+#ifelse(0<x1 & x1<1 & 0<x2 & x2<1 , 24*x1*x2, 0)}
+#w = twoDsample(f = jointPFF, N=10000,0,1,0,1)
+#g=function(x,y){x*y}
+#Expected(g,w)
+#
+# f <- function(x){
+# x1 = x[1]
+# x2 = x[2]
+# ifelse(x2>0, 1/pi/(1+x1^2) * 0.05*exp(-0.05*x2), 0)}
+# w=twoDsample(f = f, N=10000)
+#g=function(x,y){x+y}
+# 'Expected(g,w)
 
 Expected <- function(f,rv){
   L <- length(rv)
@@ -24,7 +40,7 @@ Expected <- function(f,rv){
     return(meanval)
   }
   else if (L==2){
-    meanval <- mean(f(rv$x,rv$y,na.rm=T))
+    meanval <- mean(f(rv$x,rv$y),na.rm=T)
     return(meanval)
   }
   else {
